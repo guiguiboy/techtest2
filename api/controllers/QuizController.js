088e202bcaa5559fb5815a13162819cd41d6901f
Guiguiboy <guillaume.bretou@gmail.com>
2017-09-17 23:09:22 +0200

    Added quiz controller

---
+++ api/controllers/QuizController.js
@@ -0,0 +1,17 @@
+/**
+ * QuizController
+ *
+ * @description :: Server-side logic for managing quizzes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+    home: function (req, res) {
+        return res.json({
+            todo: 'Not implemented yet!'
+        });
+    }
+};
+
